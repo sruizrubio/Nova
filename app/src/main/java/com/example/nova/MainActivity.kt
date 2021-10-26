5148c772eb1e677587ac8d16ccd8cf82f53d8f3c
Samuel Ruiz Rubio <sruizrub@everis.com>
2021-10-26 12:35:18 +0200

    Complete iteration functions

--- app/src/main/java/com/example/nova/MainActivity.kt
+++ app/src/main/java/com/example/nova/MainActivity.kt
@@ -15,43 +15,53 @@ class MainActivity : AppCompatActivity() {
         val c = Persona("", "", 190)
         val d = Persona("", "", -25)
 
-
-
-        fun contar1(): Int {
-
-            var suma = 0
-            for (i in 1..10) {
-                suma+=i
-            }
-            return suma
-        }
-
-        fun contar2(): Int{
-            var suma = 0
-            for (i in 1..10) {
-                if (i <= 6)
-                    suma+=i
-            }
-            return suma
-        }
-
-        fun contar3(): Int {
-            var suma = 0
-            for (i in 1 until 10) {
-                suma+=i
-            }
-            return suma
-        }
-
-        fun contar4(): Int{
-            var suma = 0
-
-                for(i in 1..10){
-                    suma+=i*3
-                }
-            return suma
-        }
-
         findViewById<TextView>(R.id.myText).text = contar1().toString()
+    }
+
+    fun contar1(): Int {
+
+        var suma = 0
+        for (i in 1..10) {
+            suma+=i
+        }
+        return suma
+    }
+
+    fun contar2(): Int{
+        var suma = 0
+        for (i in 1..10) {
+            suma += i
+            if (i >= 6)
+                break
+        }
+        return suma
+    }
+
+    fun contar3(): Int {
+        var suma = 0
+        for (i in 1 until 10) {
+            suma+=i
+        }
+        return suma
+    }
+
+    fun contar4(): Int{
+        var suma = 0
+        var count = 0
+        while(count < 3) {
+            for (i in 1..10) {
+                suma += i
+            }
+            count++
+        }
+        return suma
+    }
+
+    fun foreach(){
+        val list: List<String> = listOf()
+        list.forEach {
+            if(list.indexOf(it) == 2)
+                return@forEach
+        }
     }
 }
